006acd14635688679fbb33f25a8055e0a8886a65
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2021-08-29 12:04:24 +0700

    add app index

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../assets/logo.jpg";
+
+function App() {
+  return (
+    <div className="app">
+      <img className="app__logo" src={logo} alt="gpr" />
+      <h1 className="app__title">GPR's CRA Template</h1>
+    </div>
+  );
+}
+
+export default App;

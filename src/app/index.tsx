32063913d7d58ff0ad82573593d1bbe003ebba86
Gagah Pangeran Rosfatiputra <git@gagahpangeran.com>
2023-03-25 15:15:16 +0700

    add link to github repo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,6 +6,14 @@ function App() {
     <div className="app">
       <img className="app__logo" src={logo} alt="gpr" />
       <h1 className="app__title">GPR's CRA Template</h1>
+      <a
+        className="app__link"
+        href="https://github.com/gagahpangeran/gpr-cra"
+        target="_blank"
+        rel="nofollow noreferrer"
+      >
+        Use this template on Github
+      </a>
     </div>
   );
 }
